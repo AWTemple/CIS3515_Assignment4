1b07844c7d1ac641a31a42c6fe1015b9efe1b983
Aaron Wile <tuk62374@temple.edu>
2021-03-06 14:28:14 -0500

    Replaced logic for gridview instead of spinner. New Layout Fixes Alignment Issues.

--- app/src/main/java/edu/temple/cis3515_assignment4/CatAdapter.java
+++ app/src/main/java/edu/temple/cis3515_assignment4/CatAdapter.java
@@ -2,25 +2,29 @@ package edu.temple.cis3515_assignment4;
 
 import android.content.Context;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class CatAdapter extends BaseAdapter implements SpinnerAdapter
+public class CatAdapter extends BaseAdapter
 {
     Context context;
     ArrayList<Cat> cats;
+    LayoutInflater inflater;
 
     public CatAdapter (Context context, ArrayList cats)
     {
         this.context = context;
         this.cats = cats;
+        inflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -44,71 +48,18 @@ public class CatAdapter extends BaseAdapter implements SpinnerAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        //Similar to the getView method, we'll create a layout, check if
-        //converView needs to be created, and then call up the necessary data
-        LinearLayout lLayout;
-        TextView catView;
-        ImageView catImg;
+        //Inflate the convertView from the gridview_layout file
+        convertView = inflater.inflate(R.layout.gridview_layout, null);
 
-        if (convertView == null)
-        {
-            lLayout = new LinearLayout(context);
-            catView = new TextView(context);
-            catView.setTextSize(28);
-            lLayout.addView(catView);
-        }
-        else
-        {
-            lLayout = (LinearLayout)convertView;
-            catView = (TextView)lLayout.getChildAt(1);
-        }
-
-        catView.setText(cats.get(position).getName());
-        lLayout.setGravity(Gravity.CENTER_HORIZONTAL);
-        return lLayout;
-    }
-
-    @Override
-    public View getDropDownView(int position, View convertView, ViewGroup parent)
-    {
-        //Similar to the getView method, we'll create a layout, check if
-        //converView needs to be created, and then call up the necessary data
-        LinearLayout cLayout;
-        TextView catView;
-        ImageView catImg;
-
-        if (convertView == null)
-        {
-            cLayout = new LinearLayout(context);
-            catImg = new ImageView(context);
-            catView = new TextView(context);
-            catView.setTextSize(28);
-            cLayout.addView(catImg);
-            cLayout.addView(catView);
-        }
-        else
-        {
-            cLayout = (LinearLayout)convertView;
-            catImg = (ImageView)cLayout.getChildAt(0);
-            catView = (TextView)cLayout.getChildAt(1);
-        }
-
-        catImg.setAdjustViewBounds(true);
-        catImg.setMaxHeight(300);
-
-        //Set weight for space distribution
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0,300);
-        lp.weight = 1;
-        catView.setLayoutParams(lp);
-        lp.weight = 1;
-        catImg.setLayoutParams(lp);
-
+        //Prep the Image
+        ImageView catImg = convertView.findViewById(R.id.catimg);
         catImg.setImageResource(cats.get(position).getImgID());
-        catView.setText(cats.get(position).getName());
-        catView.setGravity(Gravity.CENTER_VERTICAL);
-        catView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
-        return cLayout;
+        //Prep the text
+        TextView catText = convertView.findViewById(R.id.cattext);
+        catText.setText(cats.get(position).getName());
+
+        return convertView;
     }
 
 }
--- app/src/main/java/edu/temple/cis3515_assignment4/SelectionActivity.java
+++ app/src/main/java/edu/temple/cis3515_assignment4/SelectionActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 public class SelectionActivity extends AppCompatActivity
 {
     //Classwide Declarations
-    Spinner spinner;
+    GridView gridview;
     ImageView imageView;
     TextView textView;
     int[] catImgArray;
@@ -32,21 +33,21 @@ public class SelectionActivity extends AppCompatActivity
         this.setTitle("Selection Activity");
 
         //Get our Views by ID
-        spinner = findViewById(R.id.spinner);
-        imageView = findViewById(R.id.imageView);
+        gridview = findViewById(R.id.gridView);
         textView = findViewById(R.id.prompt);
 
         //Make an ArrayList of Cat Objects
         catArray = new ArrayList<Cat>();
-        catArray.add(new Cat("Please Select a Cat:", ""));
         catArray.add(new Cat("Kitten", "I'd give it a Kit-10/10"));
         catArray.add(new Cat("Old Cat", "Aged like a fine feline."));
+        catArray.add(new Cat("Super Cat", "Birds Beware."));
         catArray.add(new Cat("Fancy Cat", "He landed on his feet."));
         catArray.add(new Cat("Buff Cat", "Meowscular."));
         catArray.add(new Cat("COVID Cat", "Sick as a Dog."));
 
+
         //Instantiate array of images to go with
-        catImgArray = new int[]{R.drawable.blank, R.drawable.kitten, R.drawable.oldcat,
+        catImgArray = new int[]{R.drawable.kitten, R.drawable.oldcat, R.drawable.supercat,
                                 R.drawable.fancycat, R.drawable.buffcat, R.drawable.covidcat};
 
         //Set Each Cat Object's Image ID
@@ -57,29 +58,17 @@ public class SelectionActivity extends AppCompatActivity
 
         //Create one of our custom catAdapters and pass it to our Spinner
         CatAdapter catAdapter = new CatAdapter(this, catArray);
-        spinner.setAdapter(catAdapter);
+        gridview.setAdapter(catAdapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener()
             {
                 @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id)
                 {
                     newActivity(position);
                 }
 
-                @Override
-                public void onNothingSelected(AdapterView<?> parent)
-                {
-                    updateScreen(0);
-                }
             });
-    }
-
-    private void updateScreen(int position)
-    {
-        Cat currCat = catArray.get(position);
-        imageView.setImageResource(currCat.getImgID());
-        textView.setText(currCat.getDesc());
     }
 
     //Added this for Assignment four:
@@ -87,15 +76,11 @@ public class SelectionActivity extends AppCompatActivity
 
     private void newActivity(int position)
     {
-        if(position == 0)
-            return; //Deal with Automatically running the blank image
-
         Cat currCat = catArray.get(position);
         Intent intent = new Intent(this, DisplayActivity.class);
         intent.putExtra(NAME, currCat.getName());
         intent.putExtra(DESC, currCat.getDesc());
         intent.putExtra(IMG, currCat.getImgID());
-        System.out.println(currCat.getName());
         startActivity(intent);
 
     }
